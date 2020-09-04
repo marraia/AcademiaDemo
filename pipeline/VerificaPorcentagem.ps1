4242441e98aad1db626ca02d60a2e2f1923d3ae4
Mendes <foes@gft.com>
2020-09-04 15:10:14 -0300

    Alterações

---
+++ pipeline/VerificaPorcentagem.ps1
@@ -0,0 +1,18 @@
+Param(
+    [string]$XmlResultado
+)
+
+Write-Host "Verificando porcentagem de cobertura de código" -ForegroundColor Green
+$xmlExecucaoCoverage = [xml](get-content $XmlResultado)
+$variavel=$xmlExecucaoCoverage.CoverageReport.Summary.Linecoverage
+$porcentagem = $variavel -replace '%'
+
+If ([double]::Parse($porcentagem) -ge 30.0)
+{
+    Write-Host "Parabéns, a cobertura de testes atingiu"$variavel "de cobertura de código!" -ForegroundColor Green
+}
+else
+{
+    Write-Host "##vso[task.logissue type=error;] Os testes não atingiram mais de 30% de cobertura de código! Está em: "$variavel
+	exit 1
+}
