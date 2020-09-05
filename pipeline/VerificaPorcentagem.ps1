6384a8f926ddfc19918536d3357325d57527f30a
Fernando Mendes <fernando.abreu@outlook.com>
2020-09-05 15:27:47 -0300

    Teste

--- pipeline/VerificaPorcentagem.ps1
+++ pipeline/VerificaPorcentagem.ps1
@@ -7,12 +7,12 @@ $xmlExecucaoCoverage = [xml](get-content $XmlResultado)
 $variavel=$xmlExecucaoCoverage.CoverageReport.Summary.Linecoverage
 $porcentagem = $variavel -replace '%'
 
-If ([double]::Parse($porcentagem) -ge 45.0)
+If ([double]::Parse($porcentagem) -ge 40.0)
 {
     Write-Host "Parabéns, a cobertura de testes atingiu"$variavel "de cobertura de código!" -ForegroundColor Green
 }
 else
 {
-    Write-Host "##vso[task.logissue type=error;] Os testes não atingiram mais de 30% de cobertura de código! Está em: "$variavel
+    Write-Host "##vso[task.logissue type=error;] Os testes não atingiram mais de 40% de cobertura de código! Está em: "$variavel
 	exit 1
 }
